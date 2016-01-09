0ec02505e287c07cd69800e24bfc430b7c53704d
Adarsha K S <ks.adarsha83@gmail.com>
2016-01-09 18:19:03 +0530

    Added ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # public-repository-info
 Get Information about any public github  repository
+
+Application URL
+------------------------------
+
+Live Application URL: https://limitless-bastion-4754.herokuapp.com/
+
+Repository Link Format: http://github.com/{user}/{repository}
+
+How Does It Work?
+------------------------------
+
+1) Uses a personal access token
+
+2) Extracts the arguments from the repository url and appends it to the github API
+
+3) Uses OAUTH to fetch results
+
+4) Using the issues_url key retrieves all the issues by using pagination
+
+Suggestions to Improve
+--------------------------------
+
+1) GitHub API by default considers an issue open even if it is a pull request. Hence we could check if the "pull_request"
+key is set to selectively choose only pure open issues. Since the requirement did not demand such things, ignored this case.
+
+2) In the above application the user has to enter the correct url (except the http or https protocol). If not it will fail
+to fetch the required results. This could be improved by providing a predictive search or by suggesting possible repository
+links by using the GitHub Repository Search API.
+
+3) Could use a deferred object to handle asynchronous requests. By this we could get rid of many if else conditions to hide or show a div.
+
+4) Could use pure javascript to intead of jquery to make it run faster.
+
--- web/assets/js/custom.js
+++ web/assets/js/custom.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
     var accessToken = 'e41855c0a7b795734624089e78c6db54176b9788';
+    var issuesPerPage = 30;
+
+    // Get the total number of pages for pagination
+    function getTotalPages(totalCount, issuesPerPage) {
+        var totalPages = Math.floor(totalCount / issuesPerPage);
+        if (totalCount % issuesPerPage > 0) {
+            totalPages++;
+        }
+        return totalPages;
+    }
+
+    // Check whether the issue is still open
+    function isIssueOpen(issue) {
+        if (issue.closed_at == undefined || issue.closed_at != null || issue.closed_at != '') {
+            return true;
+        }
+        return false;
+    }
 
     // Get the Difference between two datetime objects in hours
     function getDateDifferenceInHours(date1, date2) {
@@ -59,41 +77,43 @@ $(document).ready(function () {
                         var count4 = 0;
                         var issuesUrl = result.issues_url;
                         if (issuesUrl != '') {
+                            var totalPages = getTotalPages(count1, issuesPerPage);
+                            for (var page = 1; page <= totalPages; page++) {
+                                // Get Issues for the public repository using GitHub List Issues API
+                                url = issuesUrl.replace('{/number}', '') + '?accessToken=' + accessToken + "&page=" + page;
+                                $.ajax({
+                                    url: url,
+                                    type: 'GET',
+                                    dataType: "json",
+                                    success: function (issues) {
+                                        var today = new Date($.now());
+                                        for (var i = 0; i < issues.length; i++) {
 
-                            // Get Issues for the public repository using GitHub List Issues API
-                            url = issuesUrl.replace('{/number}', '') + '?accessToken=' + accessToken;
-                            $.ajax({
-                                url: url,
-                                type: 'GET',
-                                dataType: "json",
-                                success: function (issues) {
-                                    var today = new Date($.now());
-                                    for (var i = 0; i < issues.length; i++) {
-
-                                        // Proceed only if the created_at key is set
-                                        if (isValidCreatedAt(issues[i])) {
-                                            if (getDateDifferenceInHours(today, new Date(issues[i].created_at)) <= 24) {
-                                                count2++;
-                                            }
-                                            if (getDateDifferenceInHours(today, new Date(issues[i].created_at)) >= 24 && getDateDifferenceInDays(today, new Date(issues[i].created_at)) <= 7) {
-                                                count3++;
-                                            }
-                                            if (getDateDifferenceInDays(today, new Date(issues[i].created_at)) > 7) {
-                                                count4++;
+                                            // Proceed only if the created_at key is set
+                                            if (isValidCreatedAt(issues[i])) {
+                                                if (isIssueOpen(issues[i]) && getDateDifferenceInHours(today, new Date(issues[i].created_at)) <= 24) {
+                                                    count2++;
+                                                }
+                                                if (isIssueOpen(issues[i]) && getDateDifferenceInHours(today, new Date(issues[i].created_at)) >= 24 && getDateDifferenceInDays(today, new Date(issues[i].created_at)) <= 7) {
+                                                    count3++;
+                                                }
+                                                if (isIssueOpen(issues[i]) && getDateDifferenceInDays(today, new Date(issues[i].created_at)) > 7) {
+                                                    count4++;
+                                                }
                                             }
                                         }
+                                        $("#issues_count").text(count1);
+                                        $("#last_24").text(count2);
+                                        $("#last_24_less_7").text(count3);
+                                        $("#more_7").text(count4);
+                                        showResults();
+                                    },
+                                    error: function (jqXHR, error_type) {
+                                        message = jqXHR.statusText;
+                                        showErrorMessage();
                                     }
-                                    $("#issues_count").text(count1);
-                                    $("#last_24").text(count2);
-                                    $("#last_24_less_7").text(count3);
-                                    $("#more_7").text(count4);
-                                    showResults();
-                                },
-                                error: function (jqXHR, error_type) {
-                                    message = jqXHR.statusText;
-                                    showErrorMessage();
-                                }
-                            });
+                                });
+                            }
                         }
                     },
                     error: function (jqXHR, error_type) {
