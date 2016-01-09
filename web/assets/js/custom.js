f7242871aa95118d10242c83caf430bb7f6cdff9
Adarsha K S <ks.adarsha83@gmail.com>
2016-01-09 16:39:35 +0530

    Final Commit

---
+++ web/assets/js/custom.js
@@ -0,0 +1,112 @@
+$(document).ready(function () {
+    var accessToken = 'e41855c0a7b795734624089e78c6db54176b9788';
+
+    // Get the Difference between two datetime objects in hours
+    function getDateDifferenceInHours(date1, date2) {
+        var diffSeconds = (date1 - date2) / 1000;
+        return Math.floor(diffSeconds / 3600);
+    }
+
+    // Get the Difference between two datetime objects in days
+    function getDateDifferenceInDays(date1, date2) {
+        return Math.floor(getDateDifferenceInHours(date1, date2) / 24);
+    }
+
+    // Check whether the created_at is set and valid for the issue
+    function isValidCreatedAt(issue) {
+        if (issue.created_at != undefined && issue.created_at != null && issue.created_at != '') {
+            return true;
+        }
+        return false;
+    }
+
+    function showResults() {
+        $(".rep_info").css("display", "block");
+        $(".alert").css("display", "none");
+    }
+
+    function showErrorMessage() {
+        $(".rep_info").css("display", "none");
+        $(".alert").css("display", "block");
+    }
+
+    // Get Repository Info Handler
+    $("#get_info").on('click', function (event) {
+        event.preventDefault();
+        var message = 'Please Enter a valid Link';
+        var githubUrl = $("#github_url").val();
+        if (githubUrl != '') {
+            var githubUrlParts = githubUrl.split('github.com');
+            if (githubUrlParts.length > 1) {
+
+                // Get the Information about a public repository using GitHub Repository API
+                var url = 'https://api.github.com/repos' + githubUrlParts[1] + '?accessToken=' + accessToken;
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: "json",
+                    success: function (result) {
+                        // Total number of open issues
+                        var count1 = result.open_issues_count;
+
+                        // Number of open issues that were opened in the last 24 hours
+                        var count2 = 0;
+
+                        // Number of open issues that were opened more than 24 hours ago but less than 7 days ago
+                        var count3 = 0;
+
+                        // Number of open issues that were opened more than 7 days ago
+                        var count4 = 0;
+                        var issuesUrl = result.issues_url;
+                        if (issuesUrl != '') {
+
+                            // Get Issues for the public repository using GitHub List Issues API
+                            url = issuesUrl.replace('{/number}', '') + '?accessToken=' + accessToken;
+                            $.ajax({
+                                url: url,
+                                type: 'GET',
+                                dataType: "json",
+                                success: function (issues) {
+                                    var today = new Date($.now());
+                                    for (var i = 0; i < issues.length; i++) {
+
+                                        // Proceed only if the created_at key is set
+                                        if (isValidCreatedAt(issues[i])) {
+                                            if (getDateDifferenceInHours(today, new Date(issues[i].created_at)) <= 24) {
+                                                count2++;
+                                            }
+                                            if (getDateDifferenceInHours(today, new Date(issues[i].created_at)) >= 24 && getDateDifferenceInDays(today, new Date(issues[i].created_at)) <= 7) {
+                                                count3++;
+                                            }
+                                            if (getDateDifferenceInDays(today, new Date(issues[i].created_at)) > 7) {
+                                                count4++;
+                                            }
+                                        }
+                                    }
+                                    $("#issues_count").text(count1);
+                                    $("#last_24").text(count2);
+                                    $("#last_24_less_7").text(count3);
+                                    $("#more_7").text(count4);
+                                    showResults();
+                                },
+                                error: function (jqXHR, error_type) {
+                                    message = jqXHR.statusText;
+                                    showErrorMessage();
+                                }
+                            });
+                        }
+                    },
+                    error: function (jqXHR, error_type) {
+                        message = jqXHR.statusText;
+                        showErrorMessage();
+                    }
+                });
+            } else {
+                showErrorMessage();
+            }
+        } else {
+            showErrorMessage();
+        }
+        $("#error_message").html(message);
+    });
+});
--- web/index.php
+++ web/index.php
@@ -2,9 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Public Repository Information</title>
+    <title>Public GitHub Repository Information</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
 </head>
 <body>
+<div class="container">
+    <div class="row margin-top-20">
+        <div class="col-md-12">
+            <form>
+                <div class="input-group">
+                    <div class="input-cont">
+                        <input id="github_url" placeholder="Enter GitHub Public Repository Link..." class="form-control"
+                               type="text">
+                    </div>
+                    <div class="input-group-btn">
+                        <button type="button" class="btn btn-primary" id="get_info">
+                            Get Info &nbsp;
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-12 margin-top-20 rep_info" style="display:none;">
+            <p>Total Open Issues: <span id="issues_count"></span></p>
 
+            <p>Number of open issues that were opened in the last 24 hours: <span id="last_24"></span></p>
+
+            <p>Number of open issues that were opened more than 24 hours ago but less than 7 days ago: <span
+                    id="last_24_less_7"></span></p>
+
+            <p>Number of open issues that were opened more than 7 days ago : <span id="more_7"></span></p>
+        </div>
+        <div class="col-md-12 alert alert-danger margin-top-20" style="display:none;">
+            <span id="error_message"></span>
+        </div>
+    </div>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/custom.js"></script>
 </body>
 </html>
